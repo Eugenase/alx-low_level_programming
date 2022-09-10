799a5566ea2b569e08fc9174a47481ee2e0a22c3
Eugenase <eugenase3@gmail.com>
2022-09-10 04:42:45 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <studio.h>
 /**
- * main - Entry point
- * Return: Always 0 (Success)
+ * main - Entry Point
+ * Return: always 0 (success)
  */
 int main(void)
 {
-	printf('with proper grammar, but the otcome is a piece of art,\n");
-	return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
 }
