f0e68314aa30699a57c8978b6b7dfde015206416
Eugenase <eugenase3@gmail.com>
2022-09-21 07:43:18 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * _puts '-' prints a string
+ * _puts - prints a string
  * @str: string to print
  * Description: prints a string
  * On success: returns no error
  */
 void _puts(char *str)
 {
-	int i = 0;
-	while (*(str + i) != '\0');
-	{
-		putchar(*(str + i));
-		i++;
-	}
-	putchar(10);
+	int count;
+	for(count = 0; *s != '\0'; ++s)
+		count++;
+	return (count);
 }
