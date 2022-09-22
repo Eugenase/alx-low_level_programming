70232dd0cf8271e675ed9410e55ea7e8fe8dbaa7
Eugenase <eugenase3@gmail.com>
2022-09-22 01:04:36 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,45 +1,60 @@
-#ifndef MAIN_H
-
-#define MAIN_H
-
-#include <stdio.h>
+#include "main.h"
 
 /**
 
- *void prototypes()
+ * _strcat - function commute srtings
 
- *int prototypes()
+ * @dest: param pointer to a char
 
- *char prototypes()
+ * @src: param pointer to a char
+
+ * Return: return value of dest
 
  */
 
-void reverse_array(int *a, int n);
 
-void print_number(int n);
 
-void print_buffer(char *b, int size);
+char *_strcat(char *dest, char *src)
 
-int _strcmp(char *s1, char *s2);
+{
 
-char *_strcat(char *dest, char *src);
+	int i;
 
-char *_strncat(char *dest, char *src, int n);
-
-char *_strncpy(char *dest, char *src, int n);
-
-char *string_toupper(char *);
-
-char *cap_string(char *);
-
-char *leet(char *);
-
-char *rot13(char *);
-
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
-
-char *add_strings(char *n1, char *n2, char *r, int r_index);
+	int j;
 
 
 
-#endif /*MAIN_H*/
+	i = 0;
+
+	j = 0;
+
+
+
+	while (dest[i] != '\0')
+
+	{
+
+		i++;
+
+	}
+
+	while (src[j] != '\0')
+
+	{
+
+		dest[i] = src[j];
+
+		j++;
+
+		i++;
+
+	}
+
+
+
+	dest[i] = '\0';
+
+	return (dest);
+
+}
+
