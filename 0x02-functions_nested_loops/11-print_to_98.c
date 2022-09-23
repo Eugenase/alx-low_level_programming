346bdf960d5781e5823baea31dca68573e1601f2
Eugenase <eugenase3@gmail.com>
2022-09-23 05:59:13 -0700

    add

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,21 +1,43 @@
-#include <stdio>
-#include"main.h"
-/**
- * print_to_98 - print n to 98 counts
- * separated by comma, followed
- * by space and number should be
- * printed in order
- * @n: input
-*/
-void print_to_98(int n)
-{
-	int count;
-	if (n > 98)
-		for (count = n; count > 98; --count)
-			printf("%d, ", count);
-	else
-		for (count = n; count < 98; ++count)
+#include <stdio.h>
 
-			printf("%d, ", count);
-	printf("98\n");
+
+
+/**
+
+ * print_to_98 - Prints all natural numbers from n to 98.
+
+ * @n: Number.
+
+ */
+
+void print_to_98(int n)
+
+{
+
+	if (n >= 98)
+
+	{
+
+		while (n > 98)
+
+			printf("%d, ", n--);
+
+		printf("%d\n", n);
+
+	}
+
+
+
+	else
+
+	{
+
+		while (n < 98)
+
+			printf("%d, ", n++);
+
+		printf("%d\n", n);
+
+	}
+
 }
